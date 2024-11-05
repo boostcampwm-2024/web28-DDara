32b4ffa3b74054976ec805df7484c7fd55596473
effozen <ashgrayblue0@gmail.com>
2024-11-06 01:18:54 +0900

    [ENV][Chore] : ESLint 규칙 추가 및 세부 에러 예외 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,11 +7,15 @@ import reactPlugin from 'eslint-plugin-react';
 import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import prettierPlugin from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
+// eslint-disable-next-line import/no-unresolved
 import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+// eslint-disable-next-line import/no-unresolved
 import typescriptParser from '@typescript-eslint/parser';
+import storybook from 'eslint-plugin-storybook';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -23,6 +27,7 @@ export default [
   ...compat.config(airbnbBase),
   ...compat.config(airbnbReact),
   ...compat.config(prettierConfig),
+  ...storybook.configs['flat/recommended'],
 
   {
     languageOptions: {
@@ -32,6 +37,8 @@ export default [
     ignores: [
       'node_modules/',
       'dist/',
+      'public/', // public 폴더 제외
+      '**/*.min.js', // 모든 .min.js 파일 제외
       path.join(__dirname, 'eslint.config.mjs'), // 절대 경로로 무시
       path.join(__dirname, 'FrontEnd', 'vite.config.ts'), // 추가로 무시할 파일이 있으면 동일하게 추가
     ],
@@ -40,13 +47,29 @@ export default [
       prettier: prettierPlugin,
     },
     rules: {
-      'import/no-extraneous-dependencies': [
-        'error',
-        { devDependencies: true, packageDir: __dirname },
-      ],
+      'import/no-extraneous-dependencies': 'error',
       'import/prefer-default-export': 'off',
+      'import/no-default-export': 'warn',
       'prettier/prettier': 'error',
       'no-underscore-dangle': 'warn',
+      'no-undef': 'off',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
     },
   },
 
@@ -115,8 +138,37 @@ export default [
       'react/prop-types': 'off',
       'jsx-a11y/anchor-is-valid': 'warn',
       'react/react-in-jsx-scope': 'off',
-      'import/no-default-export': 'warn',
-      'no-undef': 'off',
+      'react/require-default-props': 'off',
+      'react/jsx-props-no-spreading': 'warn',
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.test.{js,ts,tsx}', '**/*.spec.{js,ts,tsx}'],
+    rules: {
+      'no-console': 'off', // 테스트 파일에서는 console 사용 허용
+    },
+  },
+
+  {
+    files: [
+      '**/*.stories.*', // 모든 스토리 파일
+      '**/.storybook/**/*.*', // .storybook 폴더 내 모든 파일
+      '**/eslint.config.mjs', // eslint 설정 파일
+      '**/*.test.{js,ts,tsx}', // 테스트 파일
+      '**/*.spec.{js,ts,tsx}', // 스펙 파일
+      '**/webpack.config.{js,ts}', // Webpack 설정 파일
+      '**/jest.setup.{js,ts}', // Jest 설정 파일
+      '**/vite.config.{js,ts}', // Vite 설정 파일
+      '**/*.tsdoc.{js,ts}', // TSDoc 관련 파일
+      '**/*.jsdoc.{js,ts}', // JSDoc 관련 파일
+      '**/*.vitest.{js,ts}', // Vitest 관련 파일
+      '**/swagger.config.{js,ts}', // Swagger 설정 파일
+      '**/*.swagger.{js,ts}', // Swagger 관련 파일
+    ],
+    rules: {
+      'import/no-extraneous-dependencies': 'off', // 해당 파일에서 규칙 비활성화
     },
   },
 ];
