e4f1e808e7763cc5b7fc3e7ca19ed974335e6308
effozen <ashgrayblue0@gmail.com>
2024-11-10 19:00:19 +0900

    [FE][Fix] #1 : Typescript @ 경로 인식하지 못하는 문제 해결

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths'; // 추가된 부분
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()], // tsconfigPaths 플러그인 추가
 });
