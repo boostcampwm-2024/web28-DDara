40a885f8dc2af6a7fb58d8377199aa887eb65212
juwon5272 <jackson5272@naver.com>
2024-11-13 11:34:38 +0900

    [FE][Feat] #13 : bottomsheet 드래그손잡이 구현

--- frontend/src/component/BottomSheet/BottomSheet.tsx
+++ frontend/src/component/BottomSheet/BottomSheet.tsx
@@ -16,6 +16,9 @@ export const BottomSheet = (props: IBottomSheetProps) => {
       className="fixed left-0 right-0 top-[calc(100%-90px)] z-10 flex flex-col rounded-t-lg bg-gradient-to-b shadow-lg transition-transform duration-700"
       style={{ height: `${BOTTOM_SHEET_HEIGHT}px`, background: '#FEFEFEF2' }}
     >
+      <div className="flex items-center justify-center pb-1 pt-2">
+        <div className="h-1.5 w-12 rounded-full bg-gray-300" />
+      </div>
       {props.children}
     </motion.div>
   );
