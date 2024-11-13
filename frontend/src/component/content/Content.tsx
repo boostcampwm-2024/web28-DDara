8d6635efdfee42380ed8534a0c538a32f2b659ae
juwon5272 <jackson5272@naver.com>
2024-11-13 11:19:19 +0900

    [FE][Docs] #14 : Content 컴포넌트 TSDoc 작성

--- frontend/src/component/content/Content.tsx
+++ frontend/src/component/content/Content.tsx
@@ -8,6 +8,27 @@ interface IContentProps {
   link: string;
 }
 
+/**
+ * `Content` 컴포넌트는 경로의 이름, 소요 시간, 인원 수를 표시합니다.
+ *
+ * @param {IContentProps} props - 제목, 시간, 인원, 링크 등의 속성
+ * @returns {JSX.Element} - 경로 정보를 렌더링합니다.
+ *
+ * @remarks
+ * - `person` 속성에 인원이 0명 초과인 경우 아이콘과 함께 표시됩니다.
+ * - 링크를 클릭하면 해당 경로로 이동합니다.
+ *
+ * @example
+ * ```tsx
+ * <Content
+ *   title="아들네 집으로"
+ *   time="0시간 30분"
+ *   person=2
+ *   link="/test"
+ * />
+ * ```
+ */
+
 export const Content = (props: IContentProps) => {
   return (
     <a
