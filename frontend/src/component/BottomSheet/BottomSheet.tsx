c159638bec5174aced512ebfa0cdc65a6a5c648f
juwon5272 <jackson5272@naver.com>
2024-11-13 09:42:00 +0900

    [FE][Feat] #13: bottomsheet 컴포넌트

---
+++ frontend/src/component/BottomSheet/BottomSheet.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useBottomSheet } from '@/hooks/useBottomSheet';
+import { BOTTOM_SHEET_HEIGHT } from '@/component/BottomSheet/BottomSheetOption';
+
+interface IBottomSheetProps {
+  children: React.ReactNode;
+}
+
+export const BottomSheet = (props: IBottomSheetProps) => {
+  const { sheet } = useBottomSheet();
+
+  return (
+    <motion.div
+      ref={sheet}
+      className="fixed left-0 right-0 top-[calc(100%-90px)] z-10 flex flex-col rounded-t-lg bg-gradient-to-b shadow-lg transition-transform duration-700"
+      style={{ height: `${BOTTOM_SHEET_HEIGHT}px`, background: '#FEFEFEF2' }}
+    >
+      {props.children}
+    </motion.div>
+  );
+};
