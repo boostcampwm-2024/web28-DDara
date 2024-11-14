5c6c96b2c76a85ac172ed656e6b1daafd394f5ff
이동율 <dy9623@naver.com>
2024-11-14 14:45:16 +0900

    [FE][Feat] #106 : svg 대신 react-icons 사용

--- frontend/src/pages/AddChannel.tsx
+++ frontend/src/pages/AddChannel.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { IoClose } from 'react-icons/io5';
+import { HiMiniInformationCircle } from 'react-icons/hi2';
 import { InputBox } from '../component/common/InputBox';
 
 interface IUser {
@@ -122,17 +123,8 @@ export const AddChannel = () => {
           </div>
         ))}
       </section>
-      <section className="my-4 flex flex-row items-center justify-center gap-[2px] text-xs text-gray-400">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="11px"
-          height="11px"
-          fill="black"
-          className="bi bi-info-circle-fill"
-          viewBox="0 0 16 16"
-        >
-          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
-        </svg>
+      <section className="text-grayscale-400 my-4 flex flex-row items-center justify-center gap-[2px] text-xs">
+        <HiMiniInformationCircle className="h-4 w-4" />
         사용자 별로 출발지/도착지(마커), 경로(그림)을 설정할 수 있습니다.
       </section>
       {users.length < 5 && (
