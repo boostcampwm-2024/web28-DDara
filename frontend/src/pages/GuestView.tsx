97e78405f8d549fe1e5483ec05cabd61e3104238
effozen <ashgrayblue0@gmail.com>
2024-11-14 15:59:57 +0900

    [FE][Feat] #112 : Routes 수정 및 공통 Layout에 헤더 설정

--- frontend/src/pages/GuestView.tsx
+++ frontend/src/pages/GuestView.tsx
@@ -1,31 +1,10 @@
-import { Header } from '@/component/Layout/header/Header.tsx';
-// import { MdInfo } from 'react-icons/md';
 import { NaverMap } from '@/component/maps/NaverMap.tsx';
 
 export const GuestView = () => {
-  // // TODO: 헤더 테스트 로직 추후 범용 Header로 제작시 제거 예정
-  // // Todo: 사용자 경로 받는 로직이 필요함. 우선은 하드코딩해두고 나중에 수정
-  // // Todo: Header를 좀 더 범용적으로 가져갈 필요가 있음
-  // const title = (
-  //   <h1 className="flex items-center justify-center font-normal text-gray-400">
-  //     <span className="font-semibold text-gray-900">사용자 1</span>에 대한 경로 설정
-  //   </h1>
-  // );
-  //
-  // // Todo: subtitle의 span에 대해서 묘하게 수직 정렬 안맞는 수정 필요
-  // const subTitle = (
-  //   <div className="flex items-center gap-0.5 pl-2.5 text-xs font-normal text-gray-400">
-  //     <MdInfo className="h-3 w-3 text-black" />
-  //     <span>사용자 별로 출발지/도착지(마커), 경로(그림)을 설정할 수 있습니다.</span>
-  //   </div>
-  // );
-
+  // TODO: geoCoding API를 이용해서 현재 위치나 시작위치를 기반으로 자동 좌표 설정 구현 (현재: 하드코딩)
   return (
-    <>
-      <Header />
-      <div className="h-screen">
-        <NaverMap lat={37.3595704} lng={127.105399} zoom={3} />
-      </div>
-    </>
+    <div className="h-screen">
+      <NaverMap lat={37.3595704} lng={127.105399} zoom={17} />
+    </div>
   );
 };
---
+++ frontend/src/routes/IndexRoutes.tsx
@@ -0,0 +1,39 @@
+import { Route, Routes } from 'react-router-dom';
+import { Main } from '@/pages/Main';
+import { Register } from '@/pages/Register';
+import { AddChannel } from '@/pages/AddChannel';
+import { UserRoute } from '@/pages/UserRoute';
+import { DrawRoute } from '@/pages/DrawRoute';
+import { HostView } from '@/pages/HostView';
+import { GuestView } from '@/pages/GuestView';
+import { Layout } from '@/component/Layout/Layout';
+
+export const IndexRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      {/* 메인 페이지를 위한 인덱스 라우트 */}
+      <Route index element={<Main />} />
+
+      {/* 공개 라우트 */}
+      <Route path="register" element={<Register />} />
+
+      {/* 채널 추가를 위한 중첩 라우트 */}
+      <Route path="add-channel">
+        <Route index element={<AddChannel />} />
+        <Route path=":user">
+          <Route index element={<UserRoute />} />
+          <Route path="draw" element={<DrawRoute />} />
+        </Route>
+      </Route>
+
+      {/* 채널별 뷰를 위한 중첩 라우트 */}
+      <Route path="channel/:channelId">
+        <Route path="host" element={<HostView />} />
+        <Route path="guest/:guestId" element={<GuestView />} />
+      </Route>
+
+      {/* TODO : 정의되지 않은 경로에 대한 폴백 라우트 (선택사항) */}
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Route>
+  </Routes>
+);
