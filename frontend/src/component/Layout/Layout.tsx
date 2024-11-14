b396249bc10d5056966c8cc204f39a0a37e18ee1
effozen <ashgrayblue0@gmail.com>
2024-11-14 15:34:09 +0900

    [FE][Feat] #112 : Layout의 헤더 수정

--- frontend/src/component/Layout/Layout.tsx
+++ frontend/src/component/Layout/Layout.tsx
@@ -1,37 +1,42 @@
-import React, { ReactElement } from 'react';
-import classNames from 'classnames';
-import { Header } from './Header';
+import { Outlet } from 'react-router-dom';
+import { Header } from '@/component/Layout/header/Header.tsx';
+import { MdInfo } from 'react-icons/md';
 import { Footer } from './Footer';
 
 interface ILayoutProps {
-  children: ReactElement;
-  headerTitle?: string;
   footerTitle?: string;
-  isHeaderTransparent?: boolean;
   footerActive?: boolean;
-  headerButton?: ReactElement;
   handleFooterClick?: () => void;
 }
 
-export const Layout = (props: ILayoutProps) => (
-  <div
-    className={classNames('flex', 'flex-col', 'items-center', 'w-full', 'h-full', 'bg-gray-400')}
-  >
-    {/* Header */}
-    <Header
-      title={props.headerTitle}
-      isTransparency={props.isHeaderTransparent}
-      buttonElement={props.headerButton}
-    />
+// Todo: 페이지별 다른 Header 설정을 위한 의존성에 대한 논의 및 수정 필요 (현재: 하드코딩된 값이 삽입 중)
+export const Layout = (props: ILayoutProps) => {
+  // Todo: Z-index convention에 대한 논의 필요 (현재: 하드코딩된 값)
+  return (
+    <>
+      <Header className="z-[1000]">
+        <Header.MainLayout
+          leftButton="back"
+          rightButton="dropdown"
+          title={<div>사용자 1의 시나리오</div>}
+        />
+        <Header.Subtitle>
+          <div className="flex items-center gap-0.5 pl-2.5 text-xs font-normal text-gray-400">
+            <MdInfo className="h-3 w-3 text-black" />
+            <span>사용자 별로 출발지/도착지(마커), 경로(그림)을 설정할 수 있습니다.</span>
+          </div>
+        </Header.Subtitle>
+      </Header>
 
-    {/* Main content */}
-    <main>{props.children}</main>
+      <main>
+        <Outlet />
+      </main>
 
-    {/* Footer */}
-    <Footer
-      title={props.footerTitle}
-      onClick={props.handleFooterClick}
-      active={props.footerActive}
-    />
-  </div>
-);
+      <Footer
+        title={props.footerTitle}
+        onClick={props.handleFooterClick}
+        active={props.footerActive}
+      />
+    </>
+  );
+};
