66a14e89845ba1f5db8d838abec4e6d12d841136
effozen <ashgrayblue0@gmail.com>
2024-11-18 04:25:12 +0900

    [FE][Docs] #112 : Storybook 및 TSDoc 작성

--- frontend/src/component/common/dropdown/DropdownMenu.tsx
+++ frontend/src/component/common/dropdown/DropdownMenu.tsx
@@ -3,8 +3,28 @@ import classNames from 'classnames';
 import { ToggleContext } from '@/component/common/dropdown/DropdownContext';
 
 interface IDropdownMenuProps {
+  /** 드롭다운 메뉴가 열려있는지 여부 */
   children: ReactNode | ReactNode[];
 }
+
+/**
+ * 드롭다운 메뉴 컴포넌트입니다.
+ *
+ *  @param {ReactNode} children - 드롭다운 메뉴에 들어갈 컨텐츠
+ *  @return {JSX.Element} 드롭다운 메뉴 컴포넌트
+ *
+ *  @remarks
+ *  - 드롭다운 메뉴 컴포넌트는 드롭다운 컴포넌트 내부에서 사용되어야 합니다.
+ *  - 드롭다운 메뉴 컴포넌트는 드롭다운 메뉴 역할을 수행합니다.
+ *
+ * @example
+ *  ```tsx
+ *  <Dropdown.Menu>
+ *    <Dropdown.Item>메뉴 항목 1</Dropdown.Item>
+ *    <Dropdown.Item>메뉴 항목 2</Dropdown.Item>
+ *  </Dropdown.Menu>
+ *  ```
+ */
 
 export const DropdownMenu = (props: IDropdownMenuProps) => {
   const { isOpen, toggle } = useContext(ToggleContext);
@@ -55,6 +75,7 @@ export const DropdownMenu = (props: IDropdownMenuProps) => {
           'shadow-2xl',
           'w-fit',
           'bg-white',
+          'list-none',
         )}
       >
         {props.children}
