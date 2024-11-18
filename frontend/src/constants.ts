9fd2f57c8871c2c0520b6d22ef2a3b25ef758234
happyhyep <jhi2359@naver.com>
2024-11-18 17:37:16 +0900

    [FE][Feat] #186 : 공통 함수 세팅
    - axios 관련 패키지 설치 및 세팅
    - localStorage 세팅
    - eslint max-classes-per-file 세팅 off

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -7,7 +7,8 @@ export const AppConfig = {
   /**
    * API SERVER
    */
-  API_SERVER: 'http://http://223.130.151.43:3001/api',
+  API_SERVER: 'http://223.130.151.43:3001/api',
+  // API_SERVER: 'http://localhost:3001/api',
 
   /**
    * ETC
