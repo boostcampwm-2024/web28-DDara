71c8928370f94a52d1bd3273a026cf018ebd0d93
juwon5272 <jackson5272@naver.com>
2024-11-18 18:57:48 +0900

    [FE][Feat] #4 : 캔버스에서 panning 훅

---
+++ frontend/src/hooks/usePanning.ts
@@ -0,0 +1,42 @@
+import { useRef } from 'react';
+
+interface IUsePanningProps {
+  viewPosRef: React.MutableRefObject<{ x: number; y: number }>;
+  draw: () => void;
+}
+
+const INITIAL_POSITION = { x: 0, y: 0 };
+
+export const usePanning = (props: IUsePanningProps) => {
+  const panningRef = useRef(false);
+  const startPosRef = useRef(INITIAL_POSITION);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!panningRef.current || !props.viewPosRef.current) return;
+
+    const viewPos = props.viewPosRef.current;
+    const { x, y } = startPosRef.current;
+
+    viewPos.x = e.clientX - x;
+    viewPos.y = e.clientY - y;
+
+    props.draw();
+  };
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!props.viewPosRef.current) return;
+
+    const viewPos = props.viewPosRef.current;
+    startPosRef.current = {
+      x: e.clientX - viewPos.x,
+      y: e.clientY - viewPos.y,
+    };
+    panningRef.current = true;
+  };
+
+  const handleMouseUp = () => {
+    panningRef.current = false;
+  };
+
+  return { handleMouseMove, handleMouseDown, handleMouseUp };
+};
