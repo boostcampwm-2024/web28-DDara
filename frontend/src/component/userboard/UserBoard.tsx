2e6e4bb629aad104c5a961b14c6f421e3cb0138e
이동율 <dy9623@naver.com>
2024-11-18 20:49:16 +0900

    [FE][Docs] #194 : userBoard tsDoc 작성

--- frontend/src/component/userboard/UserBoard.tsx
+++ frontend/src/component/userboard/UserBoard.tsx
@@ -25,6 +25,16 @@ const users = [
 ];
 // mock데이터 작성, 추후 context 를 이용해 dropdown과 연동
 
+/**
+ * UserBoard 컴포넌트는 지도 위 오버레이가 되며, 현재 경로의 사용자 및 선택된 사용자를 색상과 함께 표시합니다.
+ * @remarks
+ * 파라미터 혹은 전역 상태로 받은 유저를 역순으로 아래부터 보여줍니다.
+ * @example
+ * return (
+ *   <UserBoard />
+ * )
+ */
+
 export const UserBoard = () => {
   return (
     <div className="absolute bottom-2.5 right-7 z-[4050] flex flex-col gap-1.5">
