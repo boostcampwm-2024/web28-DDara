2125356ee93d956af4828c71d0b82dbe2ba3b816
Hyein Jeong <jhi2359@naver.com>
2024-11-22 00:50:33 +0900

    [FE][Feat] Main.tsx 화면에서 4주차 데모를 위한 실시간 현재 위치 받아오는 기능 추가

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,14 +1,17 @@
 export const KEYS = {
   LOGIN_USER: 'LUT',
   LOGIN_TOKEN: 'LUT_TK',
+  BROWSER_TOKEN: 'BRW_TK',
 };
 
 export const AppConfig = {
   /**
    * API SERVER
    */
-  API_SERVER: 'http://223.130.151.43:3001/api',
+  API_SERVER: 'https://ddara.kro.kr:3001/api',
   // API_SERVER: 'http://localhost:3001/api',
+  SOCKET_SERVER: 'wss://ddara.kro.kr/ws',
+  // SOCKET_SERVER: 'ws://localhost:3001',
 
   /**
    * ETC
