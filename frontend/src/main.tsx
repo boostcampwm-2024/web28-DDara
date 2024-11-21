caec07c6dbd964518625a2d058291d38d12448e5
Hyein Jeong <jhi2359@naver.com>
2024-11-21 22:51:19 +0900

    [FE][Fix] vite 경로 오류 해결

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { App } from './App';
-// 우선은 폰트 다 포함시켰는데, 나중에 사용할 것들만 따로 뺴자.
-import '@fontsource/pretendard/100.css';
-import '@fontsource/pretendard/200.css';
-import '@fontsource/pretendard/300.css';
-import '@fontsource/pretendard/400.css';
-import '@fontsource/pretendard/500.css';
-import '@fontsource/pretendard/600.css';
-import '@fontsource/pretendard/700.css';
-import '@fontsource/pretendard/800.css';
-import '@fontsource/pretendard/900.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
