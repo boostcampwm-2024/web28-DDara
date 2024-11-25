8bed5a8880105c81d7d68ed312b6c2dd9c612bbe
juwon5272 <jackson5272@naver.com>
2024-11-26 00:12:15 +0900

    [FE][Feat] #28 : 로그인 유무에 따라 바텀시트의 정보를 보여준다

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -7,33 +7,11 @@ import { FooterContext } from '@/component/layout/footer/LayoutFooterProvider';
 import { useNavigate } from 'react-router-dom';
 import { NaverMap } from '@/component/maps/NaverMapSample.tsx';
 import { buttonActiveType } from '@/component/layout/enumTypes';
-import { loadLocalData, saveLocalData } from '@/utils/common/manageLocalData.ts';
+import { loadLocalData, saveLocalData, removeLocalData } from '@/utils/common/manageLocalData.ts';
 import { AppConfig } from '@/constants.ts';
 import { v4 as uuidv4 } from 'uuid';
-
-const contentData = [
-  {
-    id: '1',
-    title: '아들네 집으로',
-    time: '0시간 34분',
-    person: 2,
-    link: '/test1',
-  },
-  {
-    id: '2',
-    title: '손자네 집으로',
-    time: '2시간 32분',
-    person: 0,
-    link: '/test2',
-  },
-  {
-    id: '3',
-    title: '마을회관으로',
-    time: '0시간 12분',
-    person: 1,
-    link: '/test3',
-  },
-];
+import { AuthModal } from '@/component/authmodal/AuthModal';
+import { getUserChannels } from '@/api/channel.api.ts';
 
 export const Main = () => {
   const { setFooterTitle, setFooterTransparency, setFooterOnClick, setFooterActive } =
@@ -41,10 +19,30 @@ export const Main = () => {
   const { lat, lng, error } = getUserLocation();
   const navigate = useNavigate();
   const [otherLocations, setOtherLocations] = useState<any[]>([]);
-  const MIN_HEIGHT = 0.5;
-  const MAX_HEIGHT = 0.8;
+  const MIN_HEIGHT = 0.35;
+  const MAX_HEIGHT = 0.9;
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
+  const [channels, setChannels] = useState<any[]>([]);
 
-  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    const token = loadLocalData(AppConfig.KEYS.LOGIN_TOKEN);
+    setIsLoggedIn(!!token);
+
+    if (token) {
+      const userId = loadLocalData(AppConfig.KEYS.LOGIN_USER);
+      if (userId) {
+        getUserChannels(userId)
+          .then(response => {
+            if (response?.data?.channels) setChannels(response.data.channels);
+          })
+          .catch(err => {
+            console.error('채널 찾기 실패 : ', err);
+          });
+      }
+    }
+  }, []);
+
   useEffect(() => {
     if (lat && lng) {
       if (!loadLocalData(AppConfig.KEYS.BROWSER_TOKEN)) {
@@ -76,6 +74,7 @@ export const Main = () => {
       };
       return () => ws.close();
     }
+    return undefined;
   }, [lat, lng]);
 
   const goToAddChannel = () => {
@@ -87,12 +86,35 @@ export const Main = () => {
     setFooterTransparency(false);
     setFooterActive(buttonActiveType.ACTIVE);
   }, []);
+
+  const handleLoginRequest = () => {
+    setShowLoginModal(true);
+  };
+
+  const handleCloseLoginModal = () => {
+    setShowLoginModal(false);
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    removeLocalData(AppConfig.KEYS.LOGIN_TOKEN);
+    removeLocalData(AppConfig.KEYS.LOGIN_USER);
+    setIsLoggedIn(!isLoggedIn);
+  };
+
+  const isUserLoggedIn = loadLocalData(AppConfig.KEYS.LOGIN_TOKEN) !== null;
+
   return (
     <div className="flex h-screen flex-col">
       <header className="absolute left-0 right-0 top-0 z-10 flex p-4">
         <button type="button" className="text-gray-700">
           <MdFormatListBulleted size={24} />
         </button>
+        {isUserLoggedIn && (
+          <button type="button" className="ml-auto text-gray-700" onClick={handleLogout}>
+            로그아웃
+          </button>
+        )}
       </header>
 
       <main
@@ -118,13 +140,29 @@ export const Main = () => {
       </main>
 
       <BottomSheet minHeight={MIN_HEIGHT} maxHeight={MAX_HEIGHT}>
-        {contentData.map(item => (
-          <Fragment key={item.id}>
-            <Content title={item.title} time={item.time} person={item.person} link={item.link} />
-            <hr className="my-2" />
-          </Fragment>
-        ))}
+        {isUserLoggedIn ? (
+          channels.map(item => (
+            <Fragment key={item.id}>
+              <Content title={item.name} time={item.time} person={item.person} link={item.link} />
+              <hr className="my-2" />
+            </Fragment>
+          ))
+        ) : (
+          <div
+            className="flex h-full cursor-pointer items-center justify-center text-center text-gray-500"
+            onClick={handleLoginRequest}
+          >
+            로그인을 진행하여
+            <br />
+            더 많은 기능을
+            <br />
+            사용해보세요
+          </div>
+        )}
       </BottomSheet>
+
+      {/* 로그인 모달 */}
+      <AuthModal isOpen={showLoginModal} onClose={handleCloseLoginModal} type="login" />
     </div>
   );
 };
