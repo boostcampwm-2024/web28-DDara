6cd67638accd4fa5b627aaccbf4381fc1733add4
Hyein Jeong <jhi2359@naver.com>
2024-11-27 00:27:53 +0900

    [FE][Feat] #286 : 데스크탑 접속시 모바일 접속 유도
    - 데스크탑 접속시 모바일 접속 유도

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,41 @@
 import { IndexRoutes } from '@/routes/IndexRoutes.tsx';
 import 'App.css';
+import { useEffect, useState } from 'react';
 
-export const App = () => <IndexRoutes />;
+export const App = () => {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      const isMobileScreen = window.innerWidth <= 768;
+
+      const userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
+      const isMobileDevice = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
+        userAgent,
+      );
+
+      setIsMobile(isMobileScreen || isMobileDevice);
+    };
+
+    checkIsMobile();
+
+    window.addEventListener('resize', checkIsMobile);
+
+    return () => window.removeEventListener('resize', checkIsMobile);
+  }, []);
+
+  if (!isMobile) {
+    return (
+      <>
+        <div className="overlay">
+          <div className="message">
+            모바일로 접속해주세요! <span>많은 관심 감사합니다.</span>
+            <span>데스크탑 버전은 추후 업데이트될 예정이니 조금만 기다려주세요!</span>
+          </div>
+        </div>
+        <IndexRoutes />
+      </>
+    );
+  }
+  return <IndexRoutes />;
+};
