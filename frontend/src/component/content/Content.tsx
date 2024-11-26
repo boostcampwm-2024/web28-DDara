d9559f7a599cb3afafa65e56cfce9be24c928bc3
juwon5272 <jackson5272@naver.com>
2024-11-26 15:05:42 +0900

    [FE][Feat] #14 : 바텀시트 내용 수정

--- frontend/src/component/content/Content.tsx
+++ frontend/src/component/content/Content.tsx
@@ -2,7 +2,6 @@ import { MdGroup, MdMoreVert } from 'react-icons/md';
 
 interface IContentProps {
   title: string;
-  time: string;
   person: number;
   link: string;
 }
@@ -31,14 +30,14 @@ interface IContentProps {
 export const Content = (props: IContentProps) => {
   return (
     <a
-      href={props.link}
+      href={`/${props.link}`}
       className="relative flex w-full flex-row items-center justify-between px-4 py-5"
     >
       <div>
         <header className="border-gray-200 pb-1 text-lg">{props.title}</header>
         <section className="flex items-center text-sm leading-5 text-gray-500">
-          <time className="mr-4">시간</time>
-          <span className="mr-6">{props.time}</span>
+          {/* <time className="mr-4">시간</time>
+          <span className="mr-6">{props.time}</span> */}
           {props.person > 0 && (
             <>
               <MdGroup className="mr-2 h-5 w-5" aria-label="인원수 아이콘" />
