b1cb5ad226cca5702faef36fdd56da3f0858ca09
Hyein Jeong <jhi2359@naver.com>
2024-11-27 15:24:46 +0900

    [FE][Refactor] #306 : 로딩 스피너 텍스트 제거
    로딩 스피너 텍스트 제거

--- frontend/src/component/common/loadingSpinner/LoadingSpinner.tsx
+++ frontend/src/component/common/loadingSpinner/LoadingSpinner.tsx
@@ -2,7 +2,6 @@ export const LoadingSpinner = () => {
   return (
     <section className="flex h-full flex-col items-center justify-center gap-2 text-xl text-gray-700">
       <div className="spinner" />
-      Loading map data...
     </section>
   );
 };
