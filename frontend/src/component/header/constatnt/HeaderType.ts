5a3310df9c61cbaaecd215ec2c6bf3cbf33d1299
이동율 <dy9623@naver.com>
2024-12-02 08:26:41 +0900

    [FE][Feat] #301 : 안쓰는 파일 정리

--- frontend/src/component/header/constatnt/HeaderType.ts
+++
@@ -1,8 +0,0 @@
-import { LuChevronLeft } from 'react-icons/lu';
-import { MdFormatListBulleted, MdLogout } from 'react-icons/md';
-
-export const HeaderIconType = {
-  BACK: LuChevronLeft,
-  MENU: MdFormatListBulleted,
-  LOGOUT: MdLogout,
-};
