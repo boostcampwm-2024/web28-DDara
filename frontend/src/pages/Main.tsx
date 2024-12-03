80add2df09722fe0d52870bd29ef0aff03d0b8f3
Hyein Jeong <jhi2359@naver.com>
2024-12-03 21:23:23 +0900

    [FE][Feat] #377 : 지도 로딩시 Loading map data... 문구 제거
    - 지도 로딩시 Loading map data... 문구 제거

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -148,7 +148,7 @@ export const Main = () => {
         ) : (
           <section className="flex h-full flex-col items-center justify-center gap-2 text-xl text-gray-700">
             <LoadingSpinner />
-            {error ? `Error: ${error}` : 'Loading map data...'}
+            {error ? `Error: ${error}` : null}
           </section>
         )}
       </main>
