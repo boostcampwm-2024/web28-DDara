320833042d3d95830a5db171fa814e8fa31e5e11
이동율 <dy9623@naver.com>
2024-12-04 21:39:29 +0900

    [FE][Feat] 아이콘 변경 및 출발지 도착지 순서 변경

--- frontend/src/component/common/types.ts
+++ frontend/src/component/common/types.ts
@@ -1,6 +1,7 @@
 import { FaPaintBrush } from 'react-icons/fa';
-import { HiLocationMarker, HiOutlineDotsHorizontal, HiOutlineLocationMarker } from 'react-icons/hi';
+import { HiLocationMarker, HiOutlineDotsHorizontal } from 'react-icons/hi';
 import { SlLayers } from 'react-icons/sl';
+import { RiFlag2Fill } from 'react-icons/ri';
 import { ButtonState } from './enums';
 
 /**
@@ -11,7 +12,7 @@ export const IconType = {
   CLOSE: SlLayers, // 닫기 버튼 아이콘
   OPEN: HiOutlineDotsHorizontal, // 열기 버튼 아이콘
   START_MARKER: HiLocationMarker, // 출발지 설정 아이콘
-  DESTINATION_MARKER: HiOutlineLocationMarker, // 도착지 설정 아이콘
+  DESTINATION_MARKER: RiFlag2Fill, // 도착지 설정 아이콘
   LINE_DRAWING: FaPaintBrush, // 경로 그리기 아이콘
 };
 /**
@@ -30,13 +31,13 @@ export const ToolCategory = [
     icon: IconType.LINE_DRAWING, // 툴 아이콘
   },
   {
-    type: ButtonState.START_MARKER, // 출발지 설정 툴
-    description: '출발지 설정', // 툴 설명
-    icon: IconType.START_MARKER, // 툴 아이콘
-  },
-  {
     type: ButtonState.DESTINATION_MARKER, // 도착지 설정 툴
     description: '도착지 설정', // 툴 설명
     icon: IconType.DESTINATION_MARKER, // 툴 아이콘
+  },
+  {
+    type: ButtonState.START_MARKER, // 출발지 설정 툴
+    description: '출발지 설정', // 툴 설명
+    icon: IconType.START_MARKER, // 툴 아이콘
   },
 ];
